a44acda9d8f7c41d980c5b1fab14bc4678bdd0db
marcosantos98 <marcomsantos98@gmail.com>
2024-11-05 12:19:28 +0000

    Complete rewrite in bun

--- index.ts
+++ index.ts
@@ -1,16 +1,8 @@
-import express, {type Response, type Request} from "express";
+import { type Server} from 'bun'
 import * as cheerio from "cheerio";
-import ky from "ky";
-
-const app = express();
 
 const EUROMILLIONS_URL = 'https://www.jogossantacasa.pt/web/SCCartazResult/'; 
 const M1LHAO_URL = 'https://www.jogossantacasa.pt/web/SCCartazResult/m1lhao';
-
-const DEBUG = false;
-
-const HOST_URL = DEBUG ? "http://localhost:3000" 
-                : "https://euro-millions-lazynessmind.vercel.app";
 
 const index = String.raw`
   <h1>Status OK</h1>
@@ -22,78 +14,77 @@ const index = String.raw`
   </ul>
 `;
 
-app.get('/', (_: Request, res: Response) => {
-  res.send(index);
-});
+export default {
+  async fetch(req: Request, _: Response) {
+    const url = new URL(req.url);
+    if (url.pathname == "/euro") {
+      const html = await (await fetch(EUROMILLIONS_URL)).text();
+      const $ = cheerio.load(html);
+      
+      let date = $('.dataInfo').text().split(' ')[7];
+              
+      let key = $('.colums').find('li').first().text().split('+');
+      let balls = key[0].trimEnd();
+      let stars = key[1].trimStart();
+      
+      let jackpot = $('.noLine').find('.stronger').last().text().trim();
+              
+      const sendKey = {
+        date: date,
+        balls: balls,
+        stars: stars,
+        jackpot: jackpot
+      };
+      
+      return Response.json(sendKey);
+    } else if (url.pathname == "/milhao") {
+      const html = await (await fetch(M1LHAO_URL)).text();
+      const $ = cheerio.load(html);
+      
+      let code = $('#code_m1').text()
+      let codeDate = $('.dataInfo').text().split(' ')[7];
+      
+      const getM1lhao = {
+        code: code,
+        code_date: codeDate
+      };
 
-interface Euro {
-  date: string,
-  balls: string,
-  stars: string,
-  jackpot: string,
-};
+      return Response.json(getM1lhao);
+    } else if (url.pathname === "/last") {
+      let html = await (await fetch(EUROMILLIONS_URL)).text();
+      let $ = cheerio.load(html);
+      
+      const date = $('.dataInfo').text().split(' ')[7];
+              
+      const key = $('.colums').find('li').first().text().split('+');
+      const balls = key[0].trimEnd();
+      const stars = key[1].trimStart();
+      
+      const jackpot = $('.noLine').find('.stronger').last().text().trim();
+      
+      html = await (await fetch(M1LHAO_URL)).text();
+      $ = cheerio.load(html);
+      
+      const code = $('#code_m1').text()
+      const codeDate = $('.dataInfo').text().split(' ')[7];
+      
+      const sendKey = {
+        date: date,
+        balls: balls,
+        stars: stars,
+        jackpot: jackpot,
+        milhaoCode: code,
+        milhaoData: codeDate,
+      };
 
-interface Milhao {
-  code: string,
-  code_date: string,
-};
-
-app.get('/last', async (_: Request, res: Response) => {
-
-  const euro = await ky.get(HOST_URL + "/euro").json<Euro>();
-  const milhao = await ky.get(HOST_URL + "/milhao").json<Milhao>();
-  
-  const sendKey = {
-      date: euro.date,
-      balls: euro.balls,
-      stars: euro.stars,
-      jackpot: euro.jackpot,
-      milhaoCode: milhao.code,
-      milhaoDate: milhao.code_date
-  };
-    
-  res.send(sendKey);      
-});
-
-app.get('/euro', async (_: Request, res: Response) => {
-  const html = await ky.get(EUROMILLIONS_URL).text();
-          
-  const $ = cheerio.load(html);
-  
-  let date = $('.dataInfo').text().split(' ')[7];
-          
-  let key = $('.colums').find('li').first().text().split('+');
-  let balls = key[0].trimEnd();
-  let stars = key[1].trimStart();
-  
-  let jackpot = $('.noLine').find('.stronger').last().text().trim();
-          
-  const sendKey = {
-    date: date,
-    balls: balls,
-    stars: stars,
-    jackpot: jackpot
-  };
-  
-  res.send(sendKey)
-});
-
-app.get('/milhao', async (_: Request, res: Response) => {  
-  const html = await ky.get(M1LHAO_URL).text();
-  
-  const $ = cheerio.load(html);
-  
-  let code = $('#code_m1').text()
-  let codeDate = $('.dataInfo').text().split(' ')[7];
-  
-  const getM1lhao = {
-    code: code,
-    code_date: codeDate
-  };
-  
-  res.send(getM1lhao);
-});
-
-app.listen(3000, () => {
-    console.log("Listenning")
-});
+      return Response.json(sendKey);
+    } else {
+      return new Response(index, {
+        status: 200,
+        headers: {
+          "Content-Type": "text/html;charset=utf-8",
+        },
+      });
+    }
+  }
+}
